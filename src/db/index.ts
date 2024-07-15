2eed87812cc5f2a16e3ffd396f93a8fea6361290
Fran Martinez <pmg.webdev@gmail.com>
2024-07-15 15:39:16 +0200

    changes port for db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,17 @@
-// Import mongoose with type definitions
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const app = express();
+
+// Use the PORT environment variable or default to 3000
+const port = process.env.PORT || 3000;
+
 // Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
-
-const MONGO_URI: string = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/massage-api";
+const MONGO_URI: string = process.env.DATABASE_URL || "mongodb://0.0.0.0:27017/massage-api";
 
 mongoose
   .connect(MONGO_URI)
@@ -18,3 +22,12 @@ mongoose
   .catch((err: Error) => {
     console.error("Error connecting to mongo: ", err);
   });
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
