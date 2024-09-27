6637b960fd544f47e8653950e340d562f86f9c42
Fran Martinez <pmg.webdev@gmail.com>
2024-09-27 11:46:35 +0200

    Add root route to app and update server URL logging

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,38 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 // ℹ️ Connects to the database
 import "./db/index";
 
 // Handles HTTP requests (express is node js framework)
-import express from 'express';
-import { json } from 'body-parser';
+import express from "express";
+import { json } from "body-parser";
 
-import authRoutes from './routes/auth.routes';
-import appointmentRoutes from './routes/appointments.routes';
-import messageRoutes from './routes/message.routes';
+import authRoutes from "./routes/auth.routes";
+import appointmentRoutes from "./routes/appointments.routes";
+import messageRoutes from "./routes/message.routes";
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-import config from './config/index';
+import config from "./config/index";
 config(app);
 
 // 👇 Apply body-parser middleware
 app.use(json());
 
+// 👇 Example root route (Welcome page)
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!");
+});
+
 // 👇 Start handling routes here
-app.use('/auth', authRoutes);
-app.use('/appointments', appointmentRoutes);
-app.use('/messages', messageRoutes);
+app.use("/auth", authRoutes);
+app.use("/appointments", appointmentRoutes);
+app.use("/messages", messageRoutes);
 
 // Error handling middleware
-import errorHandler from './error-handling/index';
+import errorHandler from "./error-handling/index";
 errorHandler(app);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
 import app from "./app";
 
-// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
+// Render dynamically assigns a port, so we should always use process.env.PORT
 const PORT: number = parseInt(process.env.PORT || "5005");
 
 app.listen(PORT, () => {
-  // Dynamically adjust URL loggin based on environment
   const env = process.env.NODE_ENV || "development";
   const url =
     env === "development"
       ? `http://localhost:${PORT}`
-      : `https://bodytreatments-backend.onrender.com`;
+      : `Render Deployment URL`;
 
-  console.log(`Server listening on ${url}`);
+  console.log(`Server is running on ${url}`);
 });
