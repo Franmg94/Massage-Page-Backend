65cc0885eca85333cc9fcf662f2496f024ee03bc
Fran Martinez <pmg.webdev@gmail.com>
2024-09-27 11:36:57 +0200

    Refactor server.ts to dynamically adjust URL logging based on environment

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import app from "./app";
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
-const PORT: number = parseInt(process.env.PORT || "5005", 10);
+const PORT: number = parseInt(process.env.PORT || "5005");
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+  // Dynamically adjust URL loggin based on environment
+  const env = process.env.NODE_ENV || "development";
+  const url =
+    env === "development"
+      ? `http://localhost:${PORT}`
+      : `https://bodytreatments-backend.onrender.com`;
+
+  console.log(`Server listening on ${url}`);
 });
